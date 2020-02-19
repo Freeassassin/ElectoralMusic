e23f6e87b0e623e1f0aeb342a248f6ad659b7bd2
Freeassassin <farbodemzadeh@gmail.com>
2020-02-19 07:50:30 -0600

    2020-02-19

--- JavaScriptCodeSnippets.js
+++ JavaScriptCodeSnippets.js
@@ -208,4 +208,36 @@ function change_chatroom(threadid)
   update();
 }
 
-//
+// loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict 
+var dropdown = document.getElementsByClassName("dropdown-btn");
+var i;
+for (i = 0; i < dropdown.length; i++) 
+{
+	dropdown[i].addEventListener("click", function() 
+	{
+		this.classList.toggle("active");
+		var dropdownContent = this.nextElementSibling;
+		if (dropdownContent.style.display === "block") 
+		{
+		  	dropdownContent.style.display = "none";
+		} 
+		else 
+		{
+		  	dropdownContent.style.display = "block";
+		}
+	});
+}
+
+// jquary code to change an elements class
+<script type="text/javascript">
+	//Show/hide the sidebar
+	$(".sidehide").on('click',function()
+	{
+	  $('.doc').toggleClass("show");
+	});
+	//Hide the sidebar
+	$(".ff").on('click',function()
+	{
+	  $('.doc').toggleClass("show");
+	});
+</script>
--- mainBody.php
+++ mainBody.php
@@ -61,64 +61,8 @@
 			?>
 		</div>
 		<script type="text/javascript">
+			 
+
+		</script>
 
 
-
-
-
- 
-function sendmsg() 
-{
-  //Send new messages to the database
-	var message = msginput.value;
-  	var threadid = getcookie("ThreadId");
-	if (message != "") 
-    {
-		var username = getcookie("messengerUname");
-		var xmlhttp=new XMLHttpRequest();
-    	msginput.value = "";
-	  	xmlhttp.open("GET","update-messages.php?username="+username+"&message=" + message+"&ThreadId="+threadid,true);
-    	xmlhttp.send();
-	}
-}
-function change_chatroom(threadid)
-{
-  //Change the the chatroom to a diffrent one
-  document.cookie="ThreadId=" + threadid;
-  update();
-}
-setInterval(function(){ update(); update_requests();update_chatrooms();}, 250);
-</script>
-<script>
-	/* Loop through all dropdown buttons to toggle between hiding and showing its dropdown content - This allows the user to have multiple dropdowns without any conflict */
-	var dropdown = document.getElementsByClassName("dropdown-btn");
-	var i;
-	for (i = 0; i < dropdown.length; i++) 
-	{
-	  dropdown[i].addEventListener("click", function() 
-	  {
-	    this.classList.toggle("active");
-	    var dropdownContent = this.nextElementSibling;
-	    if (dropdownContent.style.display === "block") 
-	    {
-	      dropdownContent.style.display = "none";
-	    } 
-	    else 
-	    {
-	      dropdownContent.style.display = "block";
-	    }
-	  });
-	}
-</script>
-<script type="text/javascript">
-	//Show/hide the sidebar
-	$(".sidehide").on('click',function()
-	{
-	  $('.doc').toggleClass("show");
-	});
-	//Hide the sidebar
-	$(".ff").on('click',function()
-	{
-	  $('.doc').toggleClass("show");
-	});
-</script>
